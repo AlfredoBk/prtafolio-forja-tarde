1908dd62bf43aef70166f3bcdc3468bba2237a79
AlfredoBk <garciabaca0711@gmail.com>
2026-02-24 17:20:06 -0300

    Add files via upload

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+        <main className="flex justify-center text-white text-lg h-screen bg-[url(/img/back.png)] bg-center bg-cover">
+            <h1 >Meus projetos</h1>
+            
+        </main>
+        )
+
+}
